a47c3bf27dd8b729ac819a2a71698c23a40368f4
royalguard14 <ghaizar.bautista@aclcbutuan.edu.ph>
2024-12-02 18:05:56 +0800

    reroute

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ session_start();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>MMRDCC</title> <!-- Set a descriptive title -->
+  <title>MMRDCCs</title> <!-- Set a descriptive title -->
   <!-- Stylesheets -->
   <link href="assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
